30dc8d92e54911801e0b1ba915fac4e50dce4cbe
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-12-30 14:50:14 +0800

    Update __init__.py

--- linalg/__init__.py
+++ linalg/__init__.py
@@ -2,7 +2,7 @@ import np
 from math import *
 from inspect import getmro as inspect
 from numpy.linalg import *
-from phyton.constants import Quantity
+from phyton.constants import rad
 
 def _check(obj, cls):
     return cls in inspect(type(obj))
@@ -14,25 +14,8 @@ def _convert(value):
     if type(value) == Scalar: return value
     else: return Scalar(value)
 
-
-def _divCross(self, other):
-    """
-    |u x v| = |u||v|sin(theta)
-    """
-    A = np.array([
-        [0, -other.z, -other.y],
-        [other.z, 0, other.x],
-        [-other.y, other.x, 0]
-    ])
-
-    if not det(A):
-        return SpatialVector()
-
-    b = np.array(self.vec)
-
-    return SpatialVector(*A.inv().dot(b))
-
 class Scalar(float): pass
+
 class SpatialVector:
     def __init__(self, x = 0, y = 0, z = 0):
         """if column:
@@ -64,7 +47,7 @@ class SpatialVector:
 
     def angleFrom(self, other):
         if _check(other, SpatialVector):
-            return acos(self.dot(other) / (self.mag *  other.mag))
+            return rad(acos(self.dot(other) / (self.mag *  other.mag)))
 
     @property
     def mag(self):
@@ -80,45 +63,34 @@ class SpatialVector:
     def __add__(self, other):
         if _check(other, SpatialVector):
             return SpatialVector(self.x + other.x, self.y + other.y, self.z + other.z)
-        else: return
+        else: return self
 
     def __radd__(self, other):
         if _check(other, SpatialVector):
             return SpatialVector(self.x + other.x , self.y + other.y , self.z + other.z)
-        else: return
+        else: return self
 
     def __sub__(self, other):
         if _check(other, SpatialVector):
             return SpatialVector(self.x - other.x , self.y - other.y , self.z - other.z)
-        else: return
+        else: return self
 
     def __rsub__(self, other):
         if _check(other, SpatialVector):
             return SpatialVector(other.x - self.x , other.y - self.y , other.z - self.z)
-        else: return
+        else: return self
 
     def __truediv__(self, other):
         if _check(other, SpatialVector):
             if other.x != 0 and other.y != 0 and other.z != 0:
                 if self.x / other.x == self.y / other.y == self.z / other.z: return self.x / other.x
-                else: return _divCross(self, other) #doesn't work, see Proof.
-            else:
-                return _divCross(self, other)
+                else: return SpatialVector(self.x / other.mag, self.y / other.mag, self.z / other.mag)
+
+            else: return SpatialVector(self.x / other.mag, self.y / other.mag, self.z / other.mag)
 
         else:
             other = _convert(other)
             return SpatialVector(self.x / other, self.y / other, self.z / other)
-
-    """
-    def __rtruediv__(self, other):
-        if _check(other, SpatialVector):
-            if self.x != 0 and self.y != 0 and self.z != 0:
-                if other.x / self.x == other.y / self.y == other.z / self.z:
-                    return other.x / self.x
-            return _divCross(other, self)
-
-        return 0
-    """
 
     def __eq__(self, other):
         if _check(other, SpatialVector):
