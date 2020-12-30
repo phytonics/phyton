a65fe334e910bae3bda2c59c676b1c1d6bb18a43
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-12-30 14:34:49 +0800

    Create Proof for cross product division impossibility.py

---
+++ linalg/Proof for cross product division impossibility.py
@@ -0,0 +1,16 @@
+self, other
+
+self = (-2, -1, 3)
+other = (1, 1, 1)
+
+A = [
+    0, -other.z, -other.y
+    other.z, 0, other.x
+    -other.y, other.x, 0
+}
+
+a, e, i = 0
+
+other.z(other.x * other.y) - other.y(other.x * other.z) = 0
+
+Therefore, this divisionCross does not work.
